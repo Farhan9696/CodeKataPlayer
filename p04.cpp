1d8e5e01d098c19214feac76f3119452d0380bde
Farhan9696 <37727645+Farhan9696@users.noreply.github.com>
2019-08-07 22:05:50 +0530

    Create p04.cpp

---
+++ p04.cpp
@@ -0,0 +1,24 @@
+#include<iostream> 
+#include<string>  
+using namespace std; 
+int main() 
+{ 
+   
+    string str; 
+  
+    
+    getline(cin,str); 
+  
+  
+    
+    str.push_back('.'); 
+  
+   
+   
+    cout << str << endl; 
+  
+  
+  
+    return 0; 
+  
+} 
